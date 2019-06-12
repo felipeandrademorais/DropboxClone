a6fcbe89b1bc214b899d2f8514196e7bc177e224
unknown <flpiandrade@gmail.com>
2019-06-12 11:52:52 -0300

    Estrutura Backend

---
+++ src/config/multer.js
@@ -0,0 +1,19 @@
+const multer = require("multer");
+const path = require("path");
+const crypto = require("crypto");
+
+module.exports = {
+  dest: path.resolve(__dirname, "..", "..", "tmp"),
+  storage: multer.diskStorage({
+    destination: (req, file, callBack) => {
+      callBack(null, path.resolve(__dirname, "..", "..", "tmp"));
+    },
+    filename: (req, file, callBack) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) callBack(err);
+        file.key = `${hash.toString("hex")}-${file.originalname}`;
+        callBack(null, file.key);
+      });
+    }
+  })
+};
